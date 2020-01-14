bceaa9e8106ae15c9a863ffa6e07a10d8e7ab4b6
Alexandru2909 <alexandruputanu@yahoo.com>
2020-01-14 21:59:58 +0200

    repaired request.py

--- Activity.py
+++ Activity.py
@@ -1,6 +1,6 @@
+import datetime
 class Activity:
     class Variable:
-        
         start=0.0
         end=0.1
         duration = 0.1
@@ -15,7 +15,8 @@ class Activity:
             self.vehicle=v
     forward = Variable()
     backward = Variable()
+    
     def __init__(self,start1,end1,execute1,vehicle1,start2,end2,execute2,vehicle2):
-        self.forward.set(start1,end1,execute1,vehicle1)
-        self.backward.set(start2,end2,execute2,vehicle2)
+        self.forward.set(datetime.timedelta(hours=int(start1.split("h")[0]), minutes=int(start1.split("h")[1])),datetime.timedelta(hours=int(end1.split("h")[0]), minutes=int(end1.split("h")[1])),execute1,vehicle1)
+        self.forward.set(datetime.timedelta(hours=int(start2.split("h")[0]), minutes=int(start2.split("h")[1])),datetime.timedelta(hours=int(end2.split("h")[0]), minutes=int(end2.split("h")[1])),execute2,vehicle2)
             
--- Request.py
+++ Request.py
@@ -1,17 +1,6 @@
 import datetime
 
 class Request:
-    class Variable:
-        start=None
-        end= None
-        duration = None
-        execute = None
-        def set(self,s,e,x,v):
-            self.start=s
-            self.end=e
-            self.duration=self.start-self.end
-            self.execute=x
-            self.vehicle=v
     # Starting place of the patient linked to request
     startPlace = None
     # Place where the care is delivered for the patient of request
@@ -28,8 +17,6 @@ class Request:
     embark = None
     #If the request was selected (1) or not (0)
     selected = 0
-    forward = Variable()
-    backward = Variable()
     def __init__(self,start,dest,ret,places,servicebegin,servicedur,category,embarkdisembark):
         self.startPlace=start
         self.destPlace=dest
@@ -51,7 +38,3 @@ class Request:
     def getEmbarkDisembark(self):
         H, M = self.embark.split("h")
         return datetime.timedelta(hours=int(H), minutes=int(M))
-
-    def setActivity(self,start1,end1,execute1,vehicle1,start2,end2,execute2,vehicle2):
-        self.forward.set(datetime.timedelta(hours=int(start1.split("h")[0]), minutes=int(start1.split("h")[1])),datetime.timedelta(hours=int(end1.split("h")[0]), minutes=int(end1.split("h")[1])),execute1,vehicle1)
-        self.forward.set(datetime.timedelta(hours=int(start2.split("h")[0]), minutes=int(start2.split("h")[1])),datetime.timedelta(hours=int(end2.split("h")[0]), minutes=int(end2.split("h")[1])),execute2,vehicle2)
